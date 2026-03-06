d0789c832c7e010d2a62088a8786b5f4a8de38af
dmartinezcs <dmartinezc4@alumnos.nebrija.es>
2026-03-06 20:55:02 +0000

    añadida la searchbar

--- my-app/src/app/components/searchBar.tsx
+++ my-app/src/app/components/searchBar.tsx
@@ -1,33 +1,16 @@
-"use client"
+type Props = {
+  search: string;
+  setSearch: (value: string) => void;
+};
 
-import { useState } from "react"
-import { Country } from "@/types/country"
-
-interface Props {
-  countries: Country[]
-  setFiltered: (countries: Country[]) => void
-}
-
-export default function SearchBar({ countries, setFiltered }: Props) {
-  const [search, setSearch] = useState<string>("")
-
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const value = e.target.value
-    setSearch(value)
-
-    const filtered = countries.filter((c) =>
-      c.name.common.toLowerCase().includes(value.toLowerCase())
-    )
-
-    setFiltered(filtered)
-  }
-
+export default function SearchBar({ search, setSearch }: Props) {
   return (
     <input
       type="text"
-      placeholder="Buscar país..."
+      placeholder="Search country..."
       value={search}
-      onChange={handleChange}
+      onChange={(e) => setSearch(e.target.value)}
+      className="border p-2 rounded w-full"
     />
-  )
+  );
 }
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -5,11 +5,13 @@ import CountryCard from "@/app/components/countryCard";
 import { Country } from "@/types/country";
 import { useEffect, useState } from "react";
 import { AxiosError } from "axios";
+import SearchBar from "@/app/components/searchBar";
 
 const Home = () => {
   const [countries, setCountries] = useState<Country[]>([]);
   const [error, setError] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     getAllCountries()
@@ -18,14 +20,20 @@ const Home = () => {
       .finally(() => setLoading(false));
   }, []);
 
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(search.toLowerCase())
+  );
+
   if (loading) return <h1>Loading...</h1>;
   if (error) return <h1>{error}</h1>;
 
   return (
     <main>
-      <h1>Countries</h1>
+      <h3>Introduce el nombre de pais que estes buscando: </h3>
+      <SearchBar search={search} setSearch={setSearch} />
+      <h1>Lista de paises</h1>
       <div className="grid">
-        {countries.map((country) => (
+        {filteredCountries.map((country) => (
           <CountryCard
             key={country.cca3 || country.name?.common}
             country={country}
